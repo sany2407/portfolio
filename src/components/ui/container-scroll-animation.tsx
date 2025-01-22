13c2b69ac9e5257631ce35e806dbbfdb86b1f83e
kaviyarasan2865 <kavikarpagam6@gmail.com>
2025-01-22 15:15:40 +0530

    12

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -57,7 +57,7 @@ export const ContainerScroll: React.FC<ContainerScrollProps> = ({
 };
 
 interface HeaderProps {
-  translate: number;
+  translate: MotionValue<number>;
   titleComponent: React.ReactNode;
 }
 
@@ -74,27 +74,31 @@ export const Header: React.FC<HeaderProps> = ({ translate, titleComponent }) => 
   );
 };
 
-export const Card = ({
-  rotate,
-  scale,
-  children,
-}: {
+interface CardProps {
   rotate: MotionValue<number>;
   scale: MotionValue<number>;
   translate: MotionValue<number>;
   children: React.ReactNode;
+}
+
+export const Card: React.FC<CardProps> = ({
+  rotate,
+  scale,
+  translate,
+  children,
 }) => {
   return (
     <motion.div
       style={{
         rotateX: rotate,
         scale,
+        translateY: translate,
         boxShadow:
           "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
       }}
       className="max-w-5xl -mt-10 mx-auto h-[30rem] md:h-[40rem] w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl"
     >
-      <div className=" h-full w-full  overflow-hidden rounded-2xl bg-gray-100 dark:bg-zinc-900 md:rounded-2xl md:p-4 ">
+      <div className="h-full w-full overflow-hidden rounded-2xl bg-gray-100 dark:bg-zinc-900 md:rounded-2xl md:p-4">
         {children}
       </div>
     </motion.div>
