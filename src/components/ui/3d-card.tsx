f62067616f078969ce21ea43640038010d694bb7
kaviyarasan2865 <kavikarpagam6@gmail.com>
2025-01-22 14:54:49 +0530

    8

--- src/components/ui/3d-card.tsx
+++ src/components/ui/3d-card.tsx
@@ -119,8 +119,7 @@ export const CardItem = ({
   rotateX?: number | string;
   rotateY?: number | string;
   rotateZ?: number | string;
-  [key: string]: any;
-}) => {
+} & React.HTMLAttributes<HTMLElement>) => {
   const ref = useRef<HTMLDivElement>(null);
   const [isMouseEntered] = useMouseEnter();
 
